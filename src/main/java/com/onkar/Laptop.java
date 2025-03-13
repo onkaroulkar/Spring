c754882c949a4088931caf5fa9552cee715986d6
onkar oulkar <onkaroulkar@gmail.com>
2025-03-13 12:12:12 +0530

    new change

--- src/main/java/com/onkar/Laptop.java
+++ src/main/java/com/onkar/Laptop.java
@@ -2,8 +2,13 @@ package com.onkar;
 
 public class Laptop implements Computer {
 
+    public Laptop(){
+        System.out.println("laptop object is created...");
+    }
+
     @Override
     public void compile(){
+
         System.out.println("compling the laptop");
     }
 }
